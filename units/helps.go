5333cf7180a8666bd8788d7345edafe588fe6c9f
coung <15555124010@163.com>
2018-12-10 23:50:25 +0800

    0.0

--- units/helps.go
+++ units/helps.go
@@ -14,8 +14,18 @@ func Recursive(Trees []datamodels.Tree, parentId int, node *List){
 	for _, val := range Trees {
 
 		if parentId == val.ParentId {
+
+
+
 			child := List{ParentId: val.ParentId, Id: val.Id, Child: []*List{}}
+
+
+
 			node.Child = append(node.Child, &child)
+
+
+
+
 			Recursive(Trees, val.ParentId, &child)
 
 		}
