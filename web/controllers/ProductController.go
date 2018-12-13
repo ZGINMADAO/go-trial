f9460f69e4236212ff5ebc5c6c98bdcfe756067d
coung <15555124010@163.com>
2018-12-13 17:50:20 +0800

    0.0

--- web/controllers/ProductController.go
+++ web/controllers/ProductController.go
@@ -36,7 +36,7 @@ func (my *ProductController) GetTypeList() {
 
 	countBuild := *build
 
-	build.Limit(SIZE, (page-1)*SIZE).Find(&product)
+	build.Limit(SIZE, (page-1)*SIZE).Desc("Id").Find(&product)
 	total, _ := (&countBuild).Count(new(datamodels.Product))
 
 	s1 := make([]datamodels.Product, 0)
@@ -46,4 +46,52 @@ func (my *ProductController) GetTypeList() {
 
 	fmt.Println(product)
 	my.Ctx.JSON(iris.Map{"rows": product, "total": total})
+}
+
+func (my *ProductController) PostType() {
+
+	title := my.Ctx.PostValueTrim("title")
+	image := my.Ctx.PostValueTrim("image")
+
+	fmt.Println(title, image)
+	if len(title) < 1 || len(image) < 1 {
+		my.Ctx.JSON(apiResource(false, nil, "名称和图片不能为空"))
+		return
+	}
+	var product datamodels.Product
+	product.Title = title
+	product.Image = image
+	my.DB.Insert(&product)
+	fmt.Println(product)
+	my.Ctx.JSON(apiResource(true, nil, "操作成功"))
+}
+
+func (my *ProductController) PutTypeBy(id int) {
+
+	title := my.Ctx.PostValueTrim("title")
+	image := my.Ctx.PostValueTrim("image")
+
+	if len(title) < 1 || len(image) < 1 {
+		my.Ctx.JSON(apiResource(false, nil, "名称和图片不能为空"))
+		return
+	}
+	product := new(datamodels.Product)
+	product.Title = title
+	product.Image = image
+	affected, _ := my.DB.Id(id).Update(product)
+	if affected < 1 {
+		my.Ctx.JSON(apiResource(false, nil, "删除失败"))
+		return
+	}
+	my.Ctx.JSON(apiResource(true, nil, "操作成功"))
+}
+
+func (my *ProductController) DeleteTypeBy(id int) {
+	product := new(datamodels.Product)
+	affected, _ := my.DB.Id(id).Delete(product)
+	if affected < 1 {
+		my.Ctx.JSON(apiResource(false, nil, "删除失败"))
+		return
+	}
+	my.Ctx.JSON(apiResource(true, nil, "操作成功"))
 }
