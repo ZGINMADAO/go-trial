0eab61ec02214a9fd0873b7da1386f721f895163
coung <15555124010@163.com>
2018-12-15 01:31:08 +0800

    0.0

--- web/controllers/OrderController.go
+++ web/controllers/OrderController.go
@@ -1,9 +1,27 @@
 package controllers
 
+import (
+	"net/smtp"
+	"strings"
+	"fmt"
+)
+
 type OrderController struct {
 	BaseController
 }
 
 func (my *OrderController) Get() {
-
+	auth := smtp.PlainAuth("", "1287020839@qq.com", "ujpltpaenazsjcae", "smtp.qq.com")
+	to := []string{"15555124010@163.com"}
+	nickname := "ggc"
+	user := "1287020839@qq.com"
+	subject := "test mail"
+	contentType := "Content-Type: text/plain; charset=UTF-8"
+	body := "This is the email body."
+	msg := []byte("To: " + strings.Join(to, ",") + "\r\nFrom: " + nickname +
+		"<" + user + ">\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + body)
+	err := smtp.SendMail("smtp.qq.com:25", auth, user, to, msg)
+	if err != nil {
+		fmt.Printf("send mail error: %v", err)
+	}
 }
