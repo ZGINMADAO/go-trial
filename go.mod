bf5d3a1939c055c5480ca7c5e24da2a98d7f5490
coung <15555124010@163.com>
2019-01-09 19:46:11 +0800

    0.0

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/Shopify/goreferrer v0.0.0-20181106222321-ec9c9a553398 // indirect
 	github.com/ajg/form v0.0.0-20160822230020-523a5da1a92f // indirect
 	github.com/aymerick/raymond v2.0.2+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/flosch/pongo2 v0.0.0-20181225140029-79872a7b2769 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/iris-contrib/formBinder v0.0.0-20171010160137-ad9fb86c356f // indirect
 	github.com/iris-contrib/go.uuid v2.0.0+incompatible // indirect
 	github.com/iris-contrib/httpexpect v0.0.0-20180314041918-ebe99fcebbce // indirect
+	github.com/iris-contrib/middleware v0.0.0-20181021162728-8bd5d51b3c2e
 	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"github.com/kataras/iris/hero"
 	"github.com/kataras/iris/websocket"
 	//"go-trial/web/socket"
+	"github.com/dgrijalva/jwt-go"
+	jwtmiddleware "github.com/iris-contrib/middleware/jwt"
 )
 
 var DB *xorm.Engine
@@ -20,6 +22,16 @@ var DB *xorm.Engine
 var SessionManage *sessions.Sessions
 
 func main() {
+
+
+
+
+	crs := func(ctx iris.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Credentials", "true")
+		ctx.Header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Content-Type")
+		ctx.Next()
+	}
 
 	SessionManage = sessions.New(sessions.Config{
 		Cookie:       "site_session_id",
@@ -30,6 +42,18 @@ func main() {
 	DB = datasource.Instance()
 
 	app := iris.New()
+
+
+	jwtHandler := jwtmiddleware.New(jwtmiddleware.Config{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte("My Secret"), nil
+		},
+		// When set, the middleware verifies that tokens are signed with the specific signing algorithm
+		// If the signing method is not constant the ValidationKeyGetter callback can be used to implement additional checks
+		// Important to avoid security issues described here: https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
 
 	app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
 		ctx.WriteString("404")
@@ -70,6 +94,7 @@ func main() {
 	mvc.Configure(app.Party("/admin"), AdminMvc)
 	mvc.Configure(app.Party("/test"), TestMvc)
 	mvc.Configure(app.Party("/socket"), SocketMvc)
+	mvc.Configure(app.Party("/wap",crs,jwtHandler.Serve), SocketMvc)
 
 	//socket.StartSocket(app)
 
@@ -127,6 +152,22 @@ func SocketMvc(app *mvc.Application) {
 }
 
 
+func WapMvc(app *mvc.Application) {
+
+	ws := websocket.New(websocket.Config{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	})
+
+	app.Router.Any("/iris-ws.js", websocket.ClientHandler())
+
+	//app.Router.Get("/echo", ws.Handler())
+	//app.Router.Get("/demo", ws.Handler())
+	//ws.OnConnection(handleConnection)
+
+	app.Register(ws.Upgrade)
+	app.Handle(new(controllers.SocketController))
+}
 
 //func handleConnection(c websocket.Connection) {
 //
